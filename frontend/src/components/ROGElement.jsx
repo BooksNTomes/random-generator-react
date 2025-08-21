e518c3082146cabeb72fb678b96cf9f1ec918176
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-08-21 15:16:22 +0800

    added comment explaining the random object generator element

--- frontend/src/components/ROGElement.jsx
+++ frontend/src/components/ROGElement.jsx
@@ -2,6 +2,23 @@
 import { useState } from "react";
 
 function ROGElement({rogelement}) {
+    /*
+        Random Object Generator Element
+        A container div that contains:
+            a generate / generate multiple button : button
+            a list of objects to choose/generate : list
+            a random number generation algorithm : from a random number generator library/s
+
+            each item in the list can be given a percentage configuration:
+            items = 1 (20%), 2(40%), 3(30%) which will set the item list to essentially have 20 1s, 40 2s, and 30 3s, setting their percentage chance to show up
+
+            algorithm is configurable through a dropdown
+            
+            button changes from generate to generate multiple depending on the boolean value.
+
+            TODO: Will need further design/decomposition based on design focus.
+    */
+
     const [outElement, setOutElement] = useState();
     const [outElements, setOutElements] = useState();
 
@@ -10,6 +27,7 @@ function ROGElement({rogelement}) {
     let algorithm = rogelement.algorithm;
     let outElementsTemp = [];
 
+    // State Handlers
     function roll(){
         switch(algorithm){
             default:
@@ -30,6 +48,7 @@ function ROGElement({rogelement}) {
         }
     }
 
+    // Return DOM
     if (displayMultBtn){
         return(
         <div className="rogelement-container">
