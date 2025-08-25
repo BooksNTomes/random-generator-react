40c0e15d69614e095b9cbb31d75161de12643d7f
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-08-25 14:10:51 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Rogelement from './components/Rogelement'
+import Rogelement from './pages/Rogelement'
 
 function App() {
   const [count, setCount] = useState(0)
