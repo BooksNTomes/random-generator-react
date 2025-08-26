959a0f2f79ad16f92e38125aae50bcdf0a0fe06d
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-08-26 21:32:12 +0800

    setting up cards

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import '../css/index.css';
+
+function Card(){
+    return (
+    <>
+        <div className="w-100 text-center p-8">
+            <img></img>
+            <h2>Lists of Objects</h2>
+            <p>Random number generators are mainly numbers, but numbers are ultimately a list of items to choose from</p>
+        </div>
+    </>
+    )
+}
+
+export default Card
--- frontend/src/components/FeatureCards.jsx
+++ frontend/src/components/FeatureCards.jsx
@@ -1,9 +1,27 @@
 import '../css/index.css'
 
 function FeatureCards(){
-    return (<>
+    return (
+    <div className="flex gap-3 items-center justify-around">
+        <div className="w-100 text-center p-8">
+            <img></img>
+            <h2>Lists of Objects</h2>
+            <p>Random number generators are mainly numbers, but numbers are ultimately a list of items to choose from</p>
+        </div>
+            
+        <div className="w-100 text-center p-8">
+            <img></img>
+            <h2>Algorithms</h2>
+            <p>Random number generators are powered by mathematical algorithms, mostly not truly random.</p>
+        </div>
 
-    </>)
+        <div className="w-100 text-center p-8">
+            <img></img>
+            <h2>Probabilities</h2>
+            <p>Each item usually have equal odds of being chosen, but probabilities can make some items more likely than others.</p>
+        </div>
+    </div>
+    )
 }
 
 export default FeatureCards
