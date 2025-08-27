502e80e744b3f4af090dd10fe67c29cf539ca490
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-08-27 16:26:38 +0800

    feature card tailwind attempts

--- frontend/src/components/FeatureCards.jsx
+++ frontend/src/components/FeatureCards.jsx
@@ -2,20 +2,32 @@ import '../css/index.css'
 
 function FeatureCards(){
     return (
-    <div className="flex gap-3 items-center justify-around">
-        <div className="w-100 text-center p-8">
+    <div className="pl-8 pr-8 flex gap-4 items-center justify-around">
+        <div className="w-100 border-2 
+                        m-0 mt-10 mb-10 
+                        text-center 
+                        p-8 min-w-80 min-h-50    rounded-[5px] shadow-md
+                        ">
             <img></img>
             <h2>Lists of Objects</h2>
             <p>Random number generators are mainly numbers, but numbers are ultimately a list of items to choose from</p>
         </div>
             
-        <div className="w-100 text-center p-8">
+        <div className="w-100 border-2 
+                        m-0 mt-10 mb-10 
+                        text-center 
+                        p-8 min-w-80 min-h-50    rounded-[5px] shadow-md
+                        ">
             <img></img>
             <h2>Algorithms</h2>
             <p>Random number generators are powered by mathematical algorithms, mostly not truly random.</p>
         </div>
 
-        <div className="w-100 text-center p-8">
+        <div className="w-100 border-2 
+                        m-0 mt-10 mb-10 
+                        text-center 
+                        p-8 min-w-80 min-h-50    rounded-[5px] shadow-md
+                        ">
             <img></img>
             <h2>Probabilities</h2>
             <p>Each item usually have equal odds of being chosen, but probabilities can make some items more likely than others.</p>
