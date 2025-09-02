579e7703086b97e22bcc1d497bb3aae93e029cff
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-09-02 10:06:40 +0800

    creating generator frontend attempt

--- frontend/src/sample_code_backup/GeneratorBackup.jsx
+++ frontend/src/sample_code_backup/GeneratorBackup.jsx
@@ -1,13 +1,13 @@
 import NavCrumbs from '../components/NavCrumbs'
 import '../css/index.css'
 
-function Generator({generator}) {
+function Generator() {
     return(
         <div className='pb-20'>
             <NavCrumbs navtarget={''}></NavCrumbs>
             <div className="browser-size m-auto">
-                <h2>{generator.name}</h2>
-                <p>{generator.description}</p>
+                <h2>generatorName</h2>
+                <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. Its purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting. Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and </p>
             </div>
             <div className="flex gap-5 browser-size m-auto p-5 border-1 border-black/15 rounded-[5px] shadow-md">
                 <div className="w-1/2">
@@ -16,16 +16,13 @@ function Generator({generator}) {
                         <h4 className="mb-3 embolden">Algorithm Config:</h4>
                         <div className="ml-5">
                             <h4 className="mb-5">Algorithm: <span>
-                               
                                 <select className="border-1 border-black/5 rounded-[5px] p-1 hover:bg-black/2">
                                     <option value="default">Default - Math.random()</option>
                                 </select>
-                                
                             </span></h4>
                         </div>
                         <h4 className="mb-3 embolden">List Config:</h4>
                         <div className="ml-5">
-
                             <h4 className="mb-5">Min: <span>
                                 <select className="border-1 border-black/5 rounded-[5px] p-1 hover:bg-black/2">
                                     <option value="default">1</option>
@@ -41,7 +38,6 @@ function Generator({generator}) {
                                     <option value="default">1</option>
                                 </select>
                             </span></h6>
-                            
                         </div>
                     </div>
                 </div>
