15b777aa4d53222e8efd76afc56625c294b9d701
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-09-18 10:33:41 +0800

    added Generator Components for refactoring purposes

---
+++ frontend/src/algorithms/algorithms.js
@@ -0,0 +1,7 @@
+export const algorithms = [
+    {   
+        type: "default",
+        name: "Default - Math.random()",
+    },
+    
+]
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -32,3 +32,13 @@ export function Footer(){
     </div>
     )
 };
+
+export function NavCrumbsRefactored({navTarget}){
+    return(
+        <div className='browser-size m-auto pt-5 mb-5'>
+            {navTarget === '' ? 
+            <h3>Generators</h3> :
+            <h3>Generators &gt; {navTarget}</h3>}
+        </div>
+    )
+}
