c898acf7e298c0fd5912834b32e241f187a025f3
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-09-24 09:48:21 +0800

    continued drafting of useAuth
    backend drafts, such as the user/auth api, and etc. are currently not tested yet.

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -8,12 +8,14 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
     const queryClient = useQueryClient();
+
     const [ token, setToken ] = useState(localStorage.getItem('token'));
     const [ user, setUser ] = useState(null);
     const [ authenticated, setAuthenticated ] = useState(false);
-    const [ isRefreshing, setIsRefreshing ] = useState(false);
-    const [ loading, setLoading ] = useState(false);
     const [ errorMessage, setErrorMessage ] = useState('');
+    
+    const [ isRefreshing, setIsRefreshing ] = useState(false);
+    const [ isLoading, setIsLoading ] = useState(false);
     
     const { data, isSuccess, error, isError, refetch: verifyToken } = useQuery({
         queryKey: ['authenticate', token],
@@ -23,22 +25,97 @@ export const AuthProvider = ({ children }) => {
     });
 
     // Refresh Access Token
-    const refreshTokenMutation = useMutation();
+    const refreshTokenMutation = useMutation({
+        mutationFn: refresh,
+        onMutate: () => {
+            setIsRefreshing(true);
+            setIsLoading(true);
+        },
+        onSuccess: (data) => {
+            setToken(data.accessToken)
+            localStorage.setItem('token', data.accessToken)
 
-    // - Recheck
-    useEffect();
+            // Enable verification query
+            setIsRefreshing(false);
+        },
+        onError: (error) => {
+            console.log('Refresh failed:', error);
+            setIsRefreshing(false);
+            setIsLoading(false);
+            logout()
+        },
+        retry: false,
+    });
+
+    // Recheck
+    useEffect(() => {
+        if(isSuccess && data) {
+            setUser(data.user);
+            setAuthenticated(true);
+            setIsLoading(false)
+        }
+
+        if(isError){
+            // If access token is invalid / expired, refresh or generate token
+            refreshTokenMutation.mutate()
+        }
+    }, [data, isError, isSuccess]);
 
     // Login Process
-    const loginMutation = useMutation();
+    const loginMutation = useMutation({
+        mutationFn: (credentials) => login(credentials),
+        onSuccess: (data) => {
+            setToken(data.credentials.token);
+            setUser({user_id: data.credentials.user_id, username: data.credentials.username});
+            localStorage.setItem('token', data.credentials.token);
+            queryClient.setQueryData(['user'], data.user);
+            setAuthenticated(true);
+            setIsLoading(false)
+            setMessageError('')
+            navigate('/generators-manager');
+        },
+        onError:(error) => {
+            setIsLoading(false);
+            setMessageError(error.message)
+        }
+    });
 
     // Register Process
-    const registerMutation = useMutation();
+    const registerMutation = useMutation({
+        mutationFn: ({ username, password }) => register(username, password),
+        onSuccess: (_data, credentials) => {
+            setMessageError('');
+            loginMutation.mutate(credentials);
+        },
+        onError: (error) => {
+            setMessageError(error.message);
+        }
+    });
 
     // Auto Log Out
-    const logout = () => {};
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        setAuthenticated(false)
+        localStorage.removeItem('token')
+    };
 
     // Return Values
-    const returnValues = {};
+    const returnValues = {
+        token,
+        authenticated,
+        user,
+        login: loginMutation.mutate,
+        loginLoading: loginMutation.isLoading,
+        isError: loginMutation.isError,
+        register: registerMutation.mutate,
+        registerLoading: registerMutation.isLoading,
+        logout,
+        isLoading,
+        errorMessage,
+        verifyToken,
+        clearError: () => setErrorMessage('')
+    };
 
     return (
         <AuthContext.Provider value={ returnValues }>
