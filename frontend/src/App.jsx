ae87029df91ab29553a4f982148225043e809851
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-11-06 10:11:34 +0800

    starting overhauls. resetting repository with pnpm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,33 @@
-import './css/index.css';
-import { Routes, Route } from 'react-router-dom';
-import { Header, Footer } from './components/Layout.jsx'
-import Home from './pages/Home';
-import About from './pages/About';
-import Generators from './pages/Generators';
-import Generator from './pages/Generator';
-import ScrollTop from './components/ScrollTop.jsx';
-import GeneratorsManager from './pages/AdminPages/GeneratorsManager.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <ScrollTop/>
-      <div className="min-h-dvh min-w-dvw flex flex-col">
-        <Header></Header>
-        <Routes className="flex-grow">
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/generators" element={<Generators/>}/>
-          <Route path="/generators/:id" element={<Generator/>}/>
-          <Route path="/generators-manager" element={<GeneratorsManager></GeneratorsManager>}/>
-        </Routes>
-        <Footer></Footer>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
